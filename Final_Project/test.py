1cd48bd89c3733796279b1ddfe174226c0f5cdad
Kimberly Jenkins <116847130+Spirit1269@users.noreply.github.com>
2025-03-28 19:35:00 -0500

    Update test.py

--- Final_Project/test.py
+++ Final_Project/test.py
@@ -1,10 +1,16 @@
 import requests
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load variables from .env into environment
+
 url2 = "https://api.yelp.com/v3/businesses/search?location=dfw&term=Starbucks&sort_by=best_match&limit=20"
+yelp_api_key = os.getenv("YELP_API_KEY")
 
 headers = {
     "accept": "application/json",
-    "authorization": "Bearer lGhHfJ5rJl8nMrr0QLei61oKEm9yS--MZa9WUtadR1-X0qWjXtvBmGRUXKqbWGwEKq6SaBgS4D69fDbKswUSFuU7ZhkLEqnXICzv8zRvfYNi5ofrJ_FZc-hQwKHhZ3Yx"
+    "authorization": yelp_api_key
 }
 
 response = requests.get(url2, headers=headers)
